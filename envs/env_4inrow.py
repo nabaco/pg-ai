515cc146cb14f0aa8db6eb557fc4d02e68d3b0be
LevZlot <lev.maloy@gmail.com>
2019-04-29 22:47:09 +0300

    Added 'envs/env_base' and 'env_4inrow'

---
+++ envs/env_4inrow.py
@@ -0,0 +1,31 @@
+from .env_base import EnvironmentBase
+
+
+class Env4InRow(EnvironmentBase):
+    """
+    An environment representing a 4-in-row game.
+    Args:
+        player1, player2: the players
+        board_size (tuple): the size of the board.
+    """
+    def __init__(self, player1, player2, board_size):
+        super(Env4InRow, self).__init__(player1, player2)
+        # TODO - implement everything!
+
+    def reset(self):
+        pass
+
+    def apply_action(self, player, action):
+        pass
+
+    def render(self):
+        pass
+
+    def available_moves(self, player):
+        pass
+
+    def is_terminal_state(self):
+        pass
+
+    def player_status(self, player):
+        pass
---
+++ envs/env_base.py
@@ -0,0 +1,56 @@
+class EnvironmentBase:
+    """
+    Base class for all environments.
+    Args:
+        player1, player2: the player objects.
+    """
+    def __init__(self, player1, player2):
+        self.player1 = player1
+        self.player2 = player2
+
+    def reset(self):
+        """
+        Resets to the initial state of the environment and returns its observation.
+        Returns:
+            An observation of the initial state.
+        """
+        raise NotImplementedError
+
+    def apply_action(self, player, action):
+        """
+        Apply action as a player.
+        Args:
+            player: the player
+            action: the action
+        Returns:
+            Reward for the action taken if the action is legal. # for now - return only 0.
+            If the action is illegal - returns None.
+        """
+        raise NotImplementedError
+
+    def render(self):
+        """
+        Prints the current state of the environment.
+        """
+        raise NotImplementedError
+
+    def available_moves(self, player):
+        """
+        Returns an iterable of available moves for the player.
+        """
+        raise NotImplementedError
+
+    def is_terminal_state(self):
+        """
+        Returns a true if the environment has reached its final state.
+        """
+        raise NotImplementedError
+
+    def player_status(self, player):
+        """
+        Returns an number representing the status of the player for this environment:
+        Negative number if the player has lost,
+        Positive number if the player has won,
+        otherwise returns 0.
+        """
+        raise NotImplementedError
