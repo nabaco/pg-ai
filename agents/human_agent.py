5482bbbb0d45fcc4895a2f333777892049165c18
David6114 <49832787+David6114@users.noreply.github.com>
2019-05-14 06:49:30 +0300

    Version 0.2 (#9)
    Human agent and random agent implementation

--- agents/human_agent.py
+++ agents/human_agent.py
@@ -1,4 +1,5 @@
 from .agent_base import Agent
+import sys
 
 
 class HumanAgent(Agent):
@@ -8,8 +9,40 @@ class HumanAgent(Agent):
     the user choose the action.
     TODO - implement everything!
     """
+
     def __init__(self, name):
         super(HumanAgent, self).__init__(name)
 
     def choose_action(self, env):
-        pass
+        # create list of available moves
+        available_moves = list(env.available_moves(self))
+
+        # check if the list are not empty
+        if available_moves:
+
+            # action of the player
+            while True:
+                print("Please choose your action from the list below and press ENTER:")
+                print(" # | col")
+                print("---------")
+                for act in enumerate(available_moves):
+                    print(" {0} |  {1}".format(act[0]+1, act[1]))
+                move = input()
+
+                # check input
+                if move == 'q':
+                    sys.exit(0)
+                try:
+                    assert int(move) > 0
+                    return available_moves[int(move)-1]
+                except (ValueError, IndexError, AssertionError) as error:
+                    if isinstance(error, ValueError):
+                        print("The input must be an integer, try again or quit (q)")
+                    elif isinstance(error, IndexError):
+                        print("The input must be from a moves list, try again or quit (q)")
+                    elif isinstance(error, AssertionError):
+                        print("The input must be a positive number, try again or quit (q)")
+                    
+
+        print("No moves available for player {}".format(self))
+        return None
--- agents/random_agent.py
+++ agents/random_agent.py
@@ -7,9 +7,12 @@ class RandomAgent(Agent):
     An agent that takes legal actions randomly.
     TODO - implement everything
     """
+
     def __init__(self, name):
         super(RandomAgent, self).__init__(name)
 
     def choose_action(self, env):
-        pass
-
+        available_moves = list(env.available_moves(self))
+        if available_moves:
+            return random.choice(available_moves)
+        return None
