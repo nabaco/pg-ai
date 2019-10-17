7fb33fd9513ec9d21ca83e6448750127424ffa74
David6114 <49832787+David6114@users.noreply.github.com>
2019-10-17 23:02:04 +0300

    K means (#40)
    * k-means implementation
    
    * k-means visualization
    
    * fix some bugs 1
    
    * empty closter implementation
    
    * K-Means test
    
    * update visualization

---
+++ models/k_means.py
@@ -0,0 +1,100 @@
+﻿from .model_base import Model
+import numpy as np
+from random import choices
+
+
+class KMeans(Model):
+    """
+    A model for K-Means clustering algorithm.
+    Arguments:
+        n_clusters (int): Num of clusters (K).
+        n_features (int): Num of the features.
+    """
+
+    def __init__(self, n_clusters, n_features):
+        self.n_clusters = n_clusters
+        self.n_features = n_features
+        self.cluster_centers = np.zeros((n_clusters, n_features))
+
+    def random_init(self, X):
+        """Random initialization of cluster centers"""
+        return np.array(choices(X, k=self.n_clusters))
+
+    def fit(self, X, n_iter, n_init, init_cluster_centers=None):
+        """
+        Training the model by dataset X.
+        Arguments:
+            X (np.ndarray): Dataset.
+            n_iter (int): Num of iteration.
+            n_init (int): Num of algorithm iteration to avoid local minimum.
+            init_cluster_centers (np.ndarray): Initial values of cluster_centers.
+        """
+        # Initialize loss value end final cluster_centers matrix
+        loss = float("inf")
+        best_cluster_centers = np.zeros((self.n_clusters, self.n_features))
+
+        # Run k_means algorithm n_init times (to avoid local minimum) and save the best result
+        for _ in range(n_init):
+            self.k_means(X, n_iter, init_cluster_centers)
+            temp_cc = self.cluster_centers
+            if self.loss(X, self.predict(X)) < loss:
+                best_cluster_centers = temp_cc
+
+        # Save the best result to the self.cluster_centers
+        self.cluster_centers = best_cluster_centers
+
+    def k_means(self, X, n_iter, init_cluster_centers):
+        """K-Means algorithm to minimize the loss function"""
+
+        # Initialization of cluster centers
+        if init_cluster_centers is None:
+            self.cluster_centers = self.random_init(X)
+        else:
+            self.cluster_centers = init_cluster_centers
+
+        for _ in range(n_iter):
+            # Associate each sample to cluster
+            labels = self.predict(X)
+
+            # Change cluster centers to the center of that samples
+            samples_sum = np.zeros((self.n_clusters, self.n_features))
+            count = np.zeros((self.n_clusters, 1))
+            for sample, label in zip(X, labels):
+                samples_sum[int(label)] += sample
+                count[int(label)] += 1
+
+            # Check if we have an empty cluster
+            if 0 in count:
+                new_center = self.farest_sample(X, count)
+                for k in range(self.n_clusters):
+                    # Assign new cluster to the farest sample from the bigest cluster
+                    if count[k, 0] == 0:
+                        self.cluster_centers[k] = new_center
+                    else:                       # Normal cluster center appdate
+                        self.cluster_centers[k] = samples_sum[k] / count[k]
+                continue
+
+            self.cluster_centers = samples_sum / count
+
+    def predict(self, X):
+        """Predict the labels of samples in the dataset X (np.ndarray)"""
+        labels = []
+        for sample in X:
+            label = min(range(self.n_clusters), key=lambda k: np.linalg.norm(
+                sample - self.cluster_centers[k])**2)
+            labels.append(label)
+        return np.array([labels]).T
+
+    def loss(self, X, labels):
+        """Compute the loss by labeles dataset X and the cluster_centers"""
+        loss = 0
+        for sample, label in zip(X, labels):
+            loss += np.linalg.norm(sample -
+                                   self.cluster_centers[int(label)])**2
+        return loss
+
+    def farest_sample(self, X, count):
+        """Return the farest sample from the bigest cluster"""
+        index = max(range(self.n_clusters), key=lambda k: int(count[k]))
+        largest_cluster = self.cluster_centers[index]
+        return max(X, key=lambda x: np.linalg.norm(x - largest_cluster))
---
+++ tests/test_k_means.py
@@ -0,0 +1,52 @@
+﻿import pytest
+import numpy as np
+from models.k_means import KMeans
+from sklearn.cluster import KMeans as SKKMeans
+
+
+ACCEPTABLE_ERROR = 1e-20
+N_ITER = 10
+N_INIT = 1
+
+
+@pytest.mark.parametrize(
+    "n_clusters, n_features, n_samples",
+    [
+        (2, 2, 200),     # Two dimension 2-Means clustering
+        (3, 3, 300),     # Three dimension 3-Means clustering
+
+    ],
+    ids=[
+        '2dim/2means',
+        '3dim/3means',
+    ])
+def test_k_means(n_clusters, n_samples, n_features):
+
+    # Initialize the models
+    cluster_centers = np.random.rand(n_clusters, n_features)
+
+    model = KMeans(n_clusters, n_features)
+    sk_model = SKKMeans(n_clusters, cluster_centers, N_INIT, N_ITER)
+
+    # Random dataset
+    X = np.random.rand(n_samples, n_features)
+    if n_clusters == 2:
+        X[n_samples//2:, :] += 0.5
+    else:
+        X[n_samples//3: 2*n_samples//3, :] += 0.5
+        X[n_samples//3: 2*n_samples//3, :] -= 0.5
+
+    # Fit the models
+    model.fit(X, N_ITER, N_INIT, cluster_centers)
+    sk_model.fit(X)
+
+    # get cluster-centers and sort them
+    centers = np.sort(model.cluster_centers, axis=0)
+    sk_centers = np.sort(sk_model.cluster_centers_, axis=0)
+
+    # Compare between the centers
+    assert centers.shape == sk_centers.shape
+    diff = 0
+    for i in (centers - sk_centers):
+        diff += np.linalg.norm(i)**2
+    assert diff < ACCEPTABLE_ERROR
